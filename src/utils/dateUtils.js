bb19f0e38aecd1a150307f3d4e52ade93545b726
Bikash Pnda <oksbwn@gmail.com>
2026-06-19 14:10:16 +0530

    Add dateUtils module

---
+++ src/utils/dateUtils.js
@@ -0,0 +1,13 @@
+/**
+ * Date formatting utility functions
+ */
+
+export function formatDate(value) {
+  if (!value) return "";
+  const date = new Date(value);
+  if (Number.isNaN(date.getTime())) return "";
+  return date.toLocaleString(undefined, {
+    dateStyle: "medium",
+    timeStyle: "short"
+  });
+}
